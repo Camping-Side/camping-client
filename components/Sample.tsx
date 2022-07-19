3401eb8bd163df1aebf011fb0230423cc3d84780
monii <moniyel@gmail.com>
2022-07-19 21:20:10 +0900

    feat: Add common component Button, Select Box, Text Field

--- components/Sample.tsx
+++ components/Sample.tsx
@@ -1,10 +1,43 @@
+/** @jsxImportSource @emotion/react */
 import React from "react";
 import { FC } from "react";
+import DefaultButton from "./common/DefaultButton";
+import DefaultSelect from "./common/DefaultSelect";
+import { SelectChangeEvent } from "@mui/material";
+import DefaultTextField from "./common/DefaultTextField";
+
+const SAMPLE_SELECT_LIST = [
+  {text: 'None', value:""},
+  {text: '10', value:"10"},
+  {text: '20', value:"20"},
+  {text: '30', value:"30"},
+]
 
 const Sample: FC = () => {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value);
+  };
+  
+
   return (
     <div>
-      Sample Component
+      <h1>Buttons</h1>
+      <section>
+      <DefaultButton  variant="text" size="small">작은 버튼</DefaultButton>
+      <DefaultButton  variant="outlined" size="medium">중간 버튼</DefaultButton>
+      <DefaultButton  variant="contained" size="large">큰 버튼</DefaultButton>
+      </section>
+      <h1>Select</h1>
+      <section>
+        <DefaultSelect selectList={SAMPLE_SELECT_LIST} label={"나이"} selected={age} handleChange={handleChange} message={"나이를 선택해 주세요"}/>
+      </section>
+      <h1>Text Field</h1>
+      <section>
+        <DefaultTextField id="outlined-error" label="이름" message="이름을 입력해주세요" value={""} error={true}/>
+        <DefaultTextField id="outlined-required" label="주소" message="주소를 입력해주세요" value={""} error={false} required={true}/>
+      </section>
     </div>
   );
 };
--- components/common/DefaultSelect.tsx
+++ components/common/DefaultSelect.tsx
@@ -1,7 +1,43 @@
 import React from 'react'
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
-export default function DefaultSelect() {
+interface ISelectItem {
+text: string
+value: string| number| undefined
+}
+interface Props {
+  selectList: ISelectItem[]
+  label: string
+  selected: string | undefined
+  error?: boolean
+  disabled?:boolean
+  message?:string
+  handleChange: (event: SelectChangeEvent) => void
+}
+
+export default function DefaultSelect({selectList, label,selected, message,handleChange}: Props) {
+
   return (
-    <div>DefaultSelect</div>
-  )
+    <div>
+      <FormControl sx={{ m: 1, minWidth: 140 }}>
+        <InputLabel id="demo-simple-select-helper-label">{label}</InputLabel>
+        <Select
+          labelId="demo-simple-select-helper-label"
+          id="demo-simple-select-helper"
+          value={selected}
+          label={label}
+          onChange={handleChange}
+        >
+          {
+            selectList.map(item => <MenuItem key={item.value} value={item.value}>{item.text}</MenuItem>)
+          }
+        </Select>
+        <FormHelperText>{message}</FormHelperText>
+      </FormControl>
+    </div>
+  );
 }
