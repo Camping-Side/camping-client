985453109e30d046287575b648d485bee29058fc
나상엽 <a02434@hanatour.com>
2022-08-12 17:47:38 +0900

    헤더 푸터 추가

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,12 +1,28 @@
-import React from "react";
-import { FC } from "react";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import MoreIcon from '@mui/icons-material/MoreVert';
 
-const Footer: FC = () => {
+export default function BottomAppBar() {
     return (
-        <footer>
-            <h2>This is Footer</h2>
-        </footer>
-    )
-}
 
-export default Footer
+            <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
+                <Toolbar>
+                    <IconButton color="inherit" aria-label="open drawer">
+                        <MenuIcon />
+                    </IconButton>
+                    <Box sx={{ flexGrow: 1 }} />
+                    <IconButton color="inherit">
+                        <SearchIcon />
+                    </IconButton>
+                    <IconButton color="inherit">
+                        <MoreIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+    );
+}
--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -1,12 +1,52 @@
-import React from "react";
-import { FC } from "react";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import SearchIcon from '@mui/icons-material/Search';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
-const Header: FC = () => {
-    return (
-        <header>
-            <h2>This is Header</h2>
-        </header>
-    )
+interface Props {
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window?: () => Window;
 }
 
-export default Header
+const navItems = ['Home', 'About', 'Contact'];
+
+export default function DrawerAppBar(props: Props) {
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
+
+    return (
+        <AppBar component="nav">
+            <Toolbar>
+                <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{flexGrow: 1, display: {xs: 'none', sm: 'block'}}}
+                >
+                    CAMPOREST LOGO
+                </Typography>
+                <Box sx={{display: {xs: 'none', sm: 'block'}}}>
+                        <Button sx={{color: '#fff'}}>
+                            <SearchIcon></SearchIcon>
+                        </Button>
+                    <Button sx={{color: '#fff'}}>
+                        <FavoriteBorderIcon></FavoriteBorderIcon>
+                    </Button>
+                    <Button sx={{color: '#fff'}}>
+                        <ShoppingBasketIcon></ShoppingBasketIcon>
+                    </Button>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+}
