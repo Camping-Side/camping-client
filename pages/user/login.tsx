996e304280d805c3347be1938a7b979d56b4d6b7
나상엽 <a02434@hanatour.com>
2022-12-01 11:32:40 +0900

    소셜로그인 버튼 컴포넌트화

--- pages/user/login.tsx
+++ pages/user/login.tsx
@@ -16,6 +16,7 @@ import {
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import styled from '@emotion/styled';
+import SocialLoginComponent from "../../components/user/SocialLogin";
 
 const Boxs = styled(Box)`
   padding-bottom: 40px !important;
@@ -130,55 +131,7 @@ const Login: FC = () => {
                     </Boxs>
                 </Box>
             </Container>
-            <Container component="main" maxWidth="xs">
-                <Box
-                    sx={{
-                        marginTop: 8,
-                    }}
-                >
-                    <Typography component="h1" variant="h5">
-                        SNS 계정으로 간편 로그인 / 회원가입
-                    </Typography>
-                    <Boxs component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            size="large"
-                        >
-                            카카오 로그인
-                        </Button>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            size="large"
-                        >
-                            네이버 로그인
-                        </Button>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            size="large"
-                        >
-                            구글 로그인
-                        </Button>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            size="large"
-                        >
-                            로그인에 어려움이 있나요?
-                        </Button>
-                    </Boxs>
-                </Box>
-            </Container>
+            <SocialLoginComponent/>
         </Layout>
     )
 }
