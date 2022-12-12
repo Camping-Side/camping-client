3eed2c1d14176db0f1446f0f18d93f440dee4f4e
나상엽 <gigic5220@gmail.com>
2022-12-12 23:22:21 +0900

    react hook form 회원가입 적용

--- pages/user/join.tsx
+++ pages/user/join.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import React, { FC, useEffect } from "react";
-import { SubmitHandler, useForm, useWatch } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import {
   Box,
   Button,
@@ -14,6 +14,7 @@ import {
 import Layout from "../../layout/Layout";
 import styled from "@emotion/styled";
 import { checkEmailDup, checkPhoneDup } from "../../actions/account";
+import { join } from "../../actions/auth";
 import { useDispatch, useSelector } from "react-redux";
 import Router from "next/router";
 import authSlice from "@reducers/auth";
@@ -45,7 +46,8 @@ const Join: FC = () => {
     watch,
     formState: { errors },
     setValue,
-    getValues,
+    clearErrors,
+    setError,
   } = useForm<Inputs>();
 
   const { joinDone } = useSelector((state: any) => state.auth);
@@ -60,6 +62,7 @@ const Join: FC = () => {
 
   useEffect(() => {
     if (joinDone) {
+      alert("회원가입되었습니다");
       dispatch(authSlice.actions.joinDone());
       Router.push("/user/login");
     }
@@ -70,6 +73,7 @@ const Join: FC = () => {
       alert("중복된 휴대폰번호입니다.");
     } else if (checkPhoneDupDone && !isPhoneDup) {
       alert("사용가능한 휴대폰번호입니다.");
+      clearErrors("phone");
     }
   }, [checkPhoneDupDone, isPhoneDup]);
 
@@ -78,6 +82,7 @@ const Join: FC = () => {
       alert("중복된 아이디(이메일)입니다.");
     } else if (checkEmailDupDone && !isEmailDup) {
       alert("사용가능한 아이디(이메일)입니다.");
+      clearErrors("email");
     }
   }, [checkEmailDupDone, isEmailDup]);
 
@@ -85,6 +90,9 @@ const Join: FC = () => {
     const phone = watch("phone");
     if (!phone) {
       alert("휴대폰번호를 입력하세요.");
+      return;
+    } else if (phone && phone.length < 10) {
+      alert("휴대폰번호를 정확히 입력하세요.");
       return;
     }
     const checkPhoneDupParam = {
@@ -96,8 +104,12 @@ const Join: FC = () => {
 
   const handleCheckEmailDup = (e: React.MouseEvent<HTMLButtonElement>) => {
     const email = watch("email");
+    const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (!email) {
       alert("아이디(이메일)을 입력하세요.");
+      return;
+    } else if (!pattern.test(email)) {
+      alert("아이디(이메일)을 정확히 입력하세요.");
       return;
     }
     const checkEmailDupParam = {
@@ -107,25 +119,17 @@ const Join: FC = () => {
     dispatch(checkEmailDup(checkEmailDupParam));
   };
 
-  /*const onSubmit = (data: any) => {
-    console.log('data: ', data)
-    // if (isPhoneDup) {
-    //   alert("휴대폰 중복확인을 해주세요.");
-    //   return;
-    // }
-    // const joinParam = {
-    //   username: name,
-    //   email: id,
-    //   password: password,
-    //   age: age,
-    //   phone: phone,
-    // };
-    // // @ts-ignore
-    // dispatch(join(joinParam));
-  };*/
-
   const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log(data);
+    const { name, email, password, phone, birth } = data;
+    const joinParam = {
+      username: name,
+      email: email,
+      password: password,
+      birth: birth,
+      phone: phone,
+    };
+    // @ts-ignore
+    dispatch(join(joinParam));
   };
 
   const errorMessage = {
@@ -133,6 +137,17 @@ const Join: FC = () => {
       return !!errors.name ? "이름을 입력하세요" : "";
     },
     phone: () => {
+      let msg = "";
+      if (errors.phone?.type === "minLength") {
+        msg = "휴대폰번호를 정확히 입력해주세요";
+      } else if (isPhoneDup) {
+        msg = "중복된 휴대폰번호입니다";
+      } else if (!!errors.phone) {
+        msg = "휴대폰번호를 입력해주세요";
+      }
+      return msg;
+    },
+    email: () => {
       let msg = "";
       if (errors.email?.type === "pattern") {
         msg = "정확한 이메일을 입력해주세요";
@@ -143,32 +158,81 @@ const Join: FC = () => {
       }
       return msg;
     },
+    password: () => {
+      let msg = "";
+      if (errors.password?.type === "minLength") {
+        msg = "비밀번호를 정확히 입력해주세요(6~12자리)";
+      } else if (watch("password") !== watch("passwordCheck")) {
+        msg = "비밀번호가 서로 일치하지 않습니다";
+      } else if (!!errors.password) {
+        msg = "비밀번호를 입력해주세요";
+      }
+      return msg;
+    },
+    passwordCheck: () => {
+      let msg = "";
+      if (errors.passwordCheck?.type === "minLength") {
+        msg = "비밀번호를 정확히 입력해주세요(6~12자리)";
+      } else if (watch("password") !== watch("passwordCheck")) {
+        msg = "비밀번호가 서로 일치하지 않습니다";
+      } else if (!!errors.passwordCheck) {
+        msg = "비밀번호를 입력해주세요";
+      }
+      return msg;
+    },
+    birth: () => {
+      let msg = "";
+      if (errors.birth?.type === "minLength") {
+        msg = "생년월일을 정확히 입력해주세요(8자리)";
+      } else if (!!errors.birth) {
+        msg = "생년월일을 입력해주세요";
+      }
+      return msg;
+    },
+  };
+
+  const serError = (item: any) => {
+    clearErrors(item);
+    if (!watch(item)) {
+      setError(item, { type: "empty", message: "empty" });
+    }
   };
 
   const formInfo = {
     name: {
       label: "이름",
-      value: { ...register("name", { required: true }) },
+      value: {
+        ...register("name", { required: true }),
+        onChange: (
+          e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+        ) => {
+          setValue("name", e.target.value.substring(0, 9));
+          serError("name");
+        },
+      },
       error: !!errors.name,
       errorMessage: errorMessage.name(),
     },
     phone: {
       label: "휴대폰번호(숫자만입력)",
       value: {
-        ...register("phone", { required: true }),
+        ...register("phone", { required: true, minLength: 10 }),
         onChange: (
           e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
         ) => {
           setValue(
             "phone",
-            e.target.value.replace(/[^0-9.]/g, "").replace(/(\..*)\./g, "$1")
+            e.target.value
+              .replace(/[^0-9.]/g, "")
+              .replace(/(\..*)\./g, "$1")
+              .substring(0, 11)
           );
+          serError("phone");
         },
       },
       error: !!errors.phone || isPhoneDup,
-      errorMessage: isPhoneDup
-        ? "중복된 휴대폰번호입니다"
-        : "휴대폰번호를 입력하세요",
+      errorMessage: errorMessage.phone(),
+      disabled: checkPhoneDupDone && !isPhoneDup,
     },
     email: {
       label: "아이디(이메일)",
@@ -177,30 +241,68 @@ const Join: FC = () => {
           required: true,
           pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
         }),
+        onChange: (
+          e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+        ) => {
+          serError("email");
+        },
       },
       error: !!errors.email || isEmailDup,
-      errorMessage: errorMessage.phone(),
+      errorMessage: errorMessage.email(),
+      disabled: checkEmailDupDone && !isEmailDup,
     },
     password: {
-      label: "비밀번호",
-      value: { ...register("password", { required: true }) },
+      label: "비밀번호(6~12자리)",
+      value: {
+        ...register("password", {
+          required: true,
+          minLength: 6,
+        }),
+        onChange: (
+          e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+        ) => {
+          setValue("password", e.target.value.substring(0, 12));
+          serError("password");
+        },
+      },
       error: !!errors.password || watch("password") !== watch("passwordCheck"),
-      errorMessage: errors.password
-        ? "비밀번호를 입력해주세요"
-        : "비밀번호가 서로 일치하지 않습니다",
+      errorMessage: errorMessage.password(),
     },
     passwordCheck: {
-      label: "비밀번호 확인",
-      value: { ...register("passwordCheck", { required: true }) },
+      label: "비밀번호 확인(6~12자리)",
+      value: {
+        ...register("passwordCheck", {
+          required: true,
+          minLength: 6,
+        }),
+        onChange: (
+          e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+        ) => {
+          setValue("passwordCheck", e.target.value.substring(0, 12));
+          serError("passwordCheck");
+        },
+      },
       error:
         !!errors.passwordCheck || watch("password") !== watch("passwordCheck"),
-      errorMessage: errors.passwordCheck
-        ? "비밀번호를 입력해주세요"
-        : "비밀번호가 서로 일치하지 않습니다",
+      errorMessage: errorMessage.passwordCheck(),
     },
     birth: {
-      label: "생년월일",
-      value: { ...register("birth", { required: true }) },
+      label: "생년월일(8자리 숫자만입력)",
+      value: {
+        ...register("birth", { required: true, minLength: 8 }),
+        onChange: (
+          e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+        ) => {
+          setValue(
+            "birth",
+            e.target.value
+              .replace(/[^0-9.]/g, "")
+              .replace(/(\..*)\./g, "$1")
+              .substring(0, 8)
+          );
+          serError("birth");
+        },
+      },
       error: !!errors.birth,
       errorMessage: !!errors.birth ? "생년월일을 입력해주세요" : "",
     },
@@ -239,6 +341,7 @@ const Join: FC = () => {
                     {...formInfo.phone.value}
                     error={formInfo.phone.error}
                     helperText={formInfo.phone.errorMessage}
+                    disabled={formInfo.phone.disabled}
                   />
                 </Grid>
                 <Grid item xs={4}>
@@ -256,6 +359,7 @@ const Join: FC = () => {
                     {...formInfo.email.value}
                     error={formInfo.email.error}
                     helperText={formInfo.email.errorMessage}
+                    disabled={formInfo.email.disabled}
                   />
                 </Grid>
                 <Grid item xs={4}>
@@ -294,22 +398,17 @@ const Join: FC = () => {
                   />
                 </Grid>
               </Grid>
-              {/*<Button
-                type="submit"
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-                size="large"
-                disabled={
-                  !checkPhoneDupDone || isPhoneDup || !checkEmailDupDone || isEmailDup
-                }
-              >
-                회원가입
-              </Button>*/}
               <Button
                 type="submit"
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
                 size="large"
+                disabled={
+                  !checkPhoneDupDone ||
+                  isPhoneDup ||
+                  !checkEmailDupDone ||
+                  isEmailDup
+                }
               >
                 회원가입
               </Button>
--- reducers/account.ts
+++ reducers/account.ts
@@ -7,10 +7,10 @@ export const initialState = {
   checkPhoneDupDone: false,
   isPhoneDup: false,
   checkPhoneDupError: null,
-    checkEmailDupLoading: false,
-    checkEmailDupDone: false,
-    isEmailDup: false,
-    checkEmailDupError: null,
+  checkEmailDupLoading: false,
+  checkEmailDupDone: false,
+  isEmailDup: false,
+  checkEmailDupError: null,
 };
 
 // toolkit 사용방법
@@ -18,32 +18,34 @@ const accountSlice = createSlice({
   name: "account",
   initialState,
   reducers: {
-      resetDupChecked(state) {
-          state.isPhoneDup = false;
-          state.checkPhoneDupDone = false;
-          state.isEmailDup = false;
-          state.checkEmailDupDone = false;
-      },
+    resetDupChecked(state) {
+      state.isPhoneDup = false;
+      state.checkPhoneDupDone = false;
+      state.isEmailDup = false;
+      state.checkEmailDupDone = false;
+    },
   },
   extraReducers: (builder) =>
     builder
-        //이메일 중복체크
-        .addCase(checkEmailDup.pending, (state) => {
-            state.checkEmailDupDone = false;
-            state.checkEmailDupLoading = true;
-            state.checkEmailDupError = null;
-        })
-        .addCase(checkEmailDup.fulfilled, (state, action) => {
-            state.checkEmailDupLoading = false;
-            state.isEmailDup = action.payload.isDup;
-            state.checkEmailDupDone = true;
-        })
-        .addCase(checkEmailDup.rejected, (state: any, action) => {
-            state.checkEmailDupLoading = false;
-            state.checkEmailDupError = action.payload;
-        })
-        //휴대폰번호 중복체크
+      //이메일 중복체크
+      .addCase(checkEmailDup.pending, (state) => {
+        state.isEmailDup = false;
+        state.checkEmailDupDone = false;
+        state.checkEmailDupLoading = true;
+        state.checkEmailDupError = null;
+      })
+      .addCase(checkEmailDup.fulfilled, (state, action) => {
+        state.checkEmailDupLoading = false;
+        state.isEmailDup = action.payload.isDup;
+        state.checkEmailDupDone = true;
+      })
+      .addCase(checkEmailDup.rejected, (state: any, action) => {
+        state.checkEmailDupLoading = false;
+        state.checkEmailDupError = action.payload;
+      })
+      //휴대폰번호 중복체크
       .addCase(checkPhoneDup.pending, (state) => {
+        state.isPhoneDup = false;
         state.checkPhoneDupDone = false;
         state.checkPhoneDupLoading = true;
         state.checkPhoneDupError = null;
