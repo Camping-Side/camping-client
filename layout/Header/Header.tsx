e3c3d8e8b0bb97e6bed99b248ea7df52d7630980
나상엽 <gigic5220@gmail.com>
2023-01-13 14:39:44 +0900

    메인화면 ui 작업 / 로고추가, 임시배너추가, 배너슬라이드 적용

--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -1,51 +1,68 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import SearchIcon from '@mui/icons-material/Search';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import SearchIcon from "@mui/icons-material/Search";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import Link from "next/link";
 import styles from "@cmStyles/module/header.module.scss";
-
-
+import CommonStyles from "@cmStyles/module/common.module.scss";
+import Logo from "/assets/img/common/logo.png";
+import Btn_search from "/assets/img/header/btn_search.png";
+import Btn_cart from "/assets/img/header/btn_cart.png";
+import Btn_like from "/assets/img/header/btn_like.png";
 
 export default function DrawerAppBar() {
-
-    return (
-        <div className={styles.appBar}>
-            <Toolbar>
-                <Typography
-                    variant="h6"
-                    component="div"
-                    sx={{
-                        flexGrow: 1,
-                    }}
-                >
-                    <a>CAMPOREST LOGO</a>
-                </Typography>
-                <Box sx={{display: {xs: 'none', sm: 'block'}}}>
-                    <Link href={'/search'}>
-                        <Button
-                            sx={{color: '#fff'}}
-                        >
-                            <SearchIcon/>
-                        </Button>
-                    </Link>
-                    <span className=""></span>
-                    <Link href={'/like'}>
-                        <Button sx={{color: '#fff'}}>
-                            <FavoriteBorderIcon/>
-                        </Button>
-                    </Link>
-                    <Link href={'/cart'}>
-                        <Button sx={{color: '#fff'}}>
-                            <ShoppingBasketIcon/>
-                        </Button>
-                    </Link>
-                </Box>
-            </Toolbar>
-        </div>
-    );
+  return (
+    <div className={styles.appBar}>
+      <Toolbar>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{
+            flexGrow: 1,
+          }}
+        >
+          <Link href={"/"}>
+            <img
+              src={Logo.src}
+              loading="lazy"
+              className={CommonStyles.pointer}
+            />
+          </Link>
+        </Typography>
+        <Box sx={{ display: { xs: "none", sm: "block" } }}>
+          <Link href={"/search"}>
+            <Button>
+              <img
+                src={Btn_search.src}
+                loading="lazy"
+                className={CommonStyles.pointer}
+              />
+            </Button>
+          </Link>
+          <Link href={"/like"}>
+            <Button>
+              <img
+                src={Btn_like.src}
+                loading="lazy"
+                className={CommonStyles.pointer}
+              />
+            </Button>
+          </Link>
+          <Link href={"/cart"}>
+            <Button>
+              <img
+                src={Btn_cart.src}
+                loading="lazy"
+                className={CommonStyles.pointer}
+              />
+            </Button>
+          </Link>
+        </Box>
+      </Toolbar>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { FC } from "react";
+import React, { FC, useState } from "react";
 
 import Layout from "@layout/Layout";
 import Link from "next/link";
@@ -7,6 +6,44 @@ import authSlice from "../reducers/auth";
 import { useDispatch, useSelector } from "react-redux";
 import BaseButton from "@cp/common/BaseButton";
 import { getInfo } from "../actions/account";
+import Grid from "@mui/material/Grid";
+
+import Banner from "/assets/img/temp/banner.png";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MobileStepper from "@mui/material/MobileStepper";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+  {
+    label: "San Francisco – Oakland Bay Bridge, United States",
+    imgPath:
+      "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
+  },
+  {
+    label: "Bird",
+    imgPath:
+      "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60",
+  },
+  {
+    label: "Bali, Indonesia",
+    imgPath:
+      "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250",
+  },
+  {
+    label: "Goč, Serbia",
+    imgPath:
+      "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60",
+  },
+];
 
 const App: FC = () => {
   const dispatch = useDispatch();
@@ -22,8 +59,89 @@ const App: FC = () => {
     dispatch(getInfo());
   };
 
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = images.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step: number) => {
+    setActiveStep(step);
+  };
+
   return (
     <Layout>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <AutoPlaySwipeableViews
+            axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+            index={activeStep}
+            onChangeIndex={handleStepChange}
+            enableMouseEvents
+          >
+            {images.map((step, index) => (
+              <div key={step.label}>
+                {Math.abs(activeStep - index) <= 2 ? (
+                  <Box
+                    component="img"
+                    sx={{
+                      height: 255,
+                      display: "block",
+                      maxWidth: 400,
+                      overflow: "hidden",
+                      width: "100%",
+                    }}
+                    src={step.imgPath}
+                    alt={step.label}
+                  />
+                ) : null}
+              </div>
+            ))}
+          </AutoPlaySwipeableViews>
+          <MobileStepper
+            steps={maxSteps}
+            position="static"
+            activeStep={activeStep}
+            nextButton={
+              <Button
+                size="small"
+                onClick={handleNext}
+                disabled={activeStep === maxSteps - 1}
+              >
+                Next
+                {theme.direction === "rtl" ? (
+                  <KeyboardArrowLeft />
+                ) : (
+                  <KeyboardArrowRight />
+                )}
+              </Button>
+            }
+            backButton={
+              <Button
+                size="small"
+                onClick={handleBack}
+                disabled={activeStep === 0}
+              >
+                {theme.direction === "rtl" ? (
+                  <KeyboardArrowRight />
+                ) : (
+                  <KeyboardArrowLeft />
+                )}
+                Back
+              </Button>
+            }
+          />
+        </Grid>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={8}></Grid>
+      </Grid>
       <Link href="/sample">샘플바로가기</Link>
       <br />
       {!loginDone && <Link href="/user/login">로그인 바로가기</Link>}
