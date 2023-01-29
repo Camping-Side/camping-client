4a97a7a9e57f7face6b3aafde76545b626cf1960
SangYeop Na <59347641+gigic5220@users.noreply.github.com>
2023-01-29 20:09:39 +0900

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,8 @@ module.exports = {
       name: "camping",
       script: "./.next/server/pages/_app.js",
       env_production: {
+        HOST: '15.165.88.20',
+        PORT: 3000,
         NODE_ENV: "production",
       },
       env_development: {
