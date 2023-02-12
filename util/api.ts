f364d472a794bb6a4784b472c570cb26234e67de
나상엽 <gigic5220@gmail.com>
2023-02-13 00:29:09 +0900

    axios 인터셉터 토큰만료시 api 재요청 수정

--- util/api.ts
+++ util/api.ts
@@ -25,42 +25,65 @@ client.interceptors.response.use(
     return response;
   },
   async function (error) {
-    if (
-      error.response &&
-      error.response.status === 401 &&
-      error.response.data.message === "Invalid password"
-    ) {
-      alert("존재하지 않는 회원정보입니다.");
-    } else if (error.response && error.response.status === 401) {
+    //400: 로그인 오류 (회원정보 불일치)
+    if (error.response && error.response.status === 400) {
+      alert(error.response.data.message);
+    }
+    //401: 권한없음
+    else if (error.response && error.response.status === 401) {
       try {
         const authInfo = localStorage.getItem("camporest_auth") || "";
-        const reissueResult = await client.post(
-          "/api/v1/auth/reissue",
-          JSON.parse(authInfo)
-        );
-        //여기서 401 검증
-        console.log("reissueResult: ", reissueResult);
-        const { data } = reissueResult;
-        if (data && data.statusCode === 200) {
-          localStorage.removeItem("camporest_auth");
-          localStorage.setItem(
-            "camporest_auth",
-            JSON.stringify(data.resultData)
-          );
-          const { config } = error;
-          const axiosRequestConfig = {
-            url: config.url,
-            method: config.method,
+        //localStorage에 토큰이 있는 경우 (로그인이 되어있는 상태지만 accessToken이 만료됐을 경우)
+        if (authInfo) {
+          const res = await fetch(BASE_URL + "/api/v1/auth/reissue", {
+            method: "POST",
             headers: {
-              Authorization: "Bearer " + data.resultData.accessToken,
+              "Content-Type": "application/json",
             },
-          };
-          return await client.request(axiosRequestConfig);
+            body: authInfo,
+          });
+          const resData = await res.json();
+          //refresh토큰이 정상일 경우 => accessToken재발급 후 원래 api재요청
+          if (res.ok && res.status === 200) {
+            localStorage.removeItem("camporest_auth");
+            localStorage.setItem(
+              "camporest_auth",
+              JSON.stringify(resData.resultData)
+            );
+            const { config } = error;
+            const axiosRequestConfig = {
+              url: config.url,
+              method: config.method,
+              headers: {
+                Authorization: "Bearer " + resData.resultData.accessToken,
+              },
+            };
+            const reissueData = await client.request(axiosRequestConfig);
+            return reissueData;
+          }
+          //refresh토큰이 만료됐을 경우 => 재로그인
+          else if (!res.ok) {
+            alert("로그인이 필요합니다.");
+            location.href = "/user/login";
+          }
+        }
+        //localStorage에 토큰이 없을 경우
+        else {
+          alert("로그인이 필요합니다.");
+          location.href = "/user/login";
         }
       } catch (error) {
-        alert("재로그인이 필요합니다.");
+        alert("로그인이 필요합니다.");
         location.href = "/";
       }
+    }
+    //403 관리자 권한의 api를 요청하는 경우
+    else if (error.response && error.response.status === 403) {
+      alert("권한이 없습니다.");
+      location.href = "/user/login";
+    } else {
+      alert("관리자에게 문의해주세요. gigic5220@gmail.com");
+      location.href = "/";
     }
   }
 );
