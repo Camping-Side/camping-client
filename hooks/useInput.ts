91f4e1306f63d803f35bb3228b832ef19f03270b
나상엽 <gigic5220@gmail.com>
2023-02-15 16:44:42 +0900

    컴파일 오류 수정

--- hooks/useInput.ts
+++ hooks/useInput.ts
@@ -3,7 +3,7 @@ import { useState, useCallback } from "react";
 function useInputs(initialForm: any) {
   const [form, setForm] = useState(initialForm);
   // change
-  const onChange = useCallback((e) => {
+  const onChange = useCallback((e: any) => {
     const { name, value } = e.target;
     setForm((form: any) => ({ ...form, [name]: value }));
   }, []);
